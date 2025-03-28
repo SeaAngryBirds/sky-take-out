db81111e5a933f26839ea5d66a65c4feac339b5b
Heisenberg <o.angry.birds.oo@gmail.com>
2025-03-28 21:00:23 +0800

    编辑员工

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -91,6 +91,22 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     @Override
+    public Employee getById(Long id) {
+        Employee employee = employeeMapper.getById(id);
+        employee.setPassword("****");
+        return employee;
+    }
+
+    @Override
+    public void update(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeDTO, employee);
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser(BaseContext.getCurrentId());
+        employeeMapper.update(employee);
+    }
+
+    @Override
     public void startOrStop(Integer status, Long id) {
         Employee employee = Employee.builder().id(id).status(status).build();
         employeeMapper.update(employee);
